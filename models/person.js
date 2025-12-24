81517af1d4bacb20ee4a1b2d0b454e526ecfa334
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-12-24 10:59:40 -0500

    feat: exercise 3.20 - add custom phone number validation

--- models/person.js
+++ models/person.js
@@ -21,7 +21,18 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number! Use format XX-XXXXXXX`,
+    },
+  },
 });
 
 // Transformamos la respuesta para limpiar el objeto que devuelve Mongo
